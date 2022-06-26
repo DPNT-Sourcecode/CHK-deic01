7e0a56e139314f413b5acc70d30aaf23c8e8bc0e
sbx_user1051 <sbx_user1051@169.254.21.173>
2022-06-26 11:41:19 +0000

    Sun Jun 26 11:41:19 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -28,20 +28,21 @@ def checkout(skus):
     price = 0
     count = Counter(skus)
     for sku in FREE_OFFERS:
-        if count[sku] > FREE_OFFERS[sku][0]:
+        while count[sku] >= FREE_OFFERS[sku][0]:
+            price += PRICE_TABLE[sku] * count[sku]
             count[sku] -= FREE_OFFERS[sku][0]
             count[FREE_OFFERS[sku][1]] -= FREE_OFFERS[sku][2]
+
     for sku in count:
         if sku in OFFERS:
-            while count[sku] >= min(OFFERS[sku].keys():
-                price += OFFERS[sku][1]
-                count[sku] -= OFFERS[sku][0]
+            while count[sku] >= min(OFFERS[sku].keys()):
+                offer_count = max(
+                    [key for key in OFFERS[sku].keys() if key <= count[sku]]
+                )
+                price += OFFERS[sku][offer_count]
+                count[sku] -= offer_count
         try:
             price += PRICE_TABLE[sku] * count[sku]
         except KeyError:
             return -1
     return price
-
-
-
-
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -5,5 +5,3 @@ class TestCheckout:
     def test_checkout(self):
         assert checkout_solution.checkout("ABCDABCD") == 215
         assert checkout_solution.checkout("AAAAAAAAAEEBBB") == 505
-
-
