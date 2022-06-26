18c64be35bca45d42cda52193c8d2ecd0db172a0
sbx_user1051 <sbx_user1051@169.254.21.173>
2022-06-26 11:15:42 +0000

    Sun Jun 26 11:15:42 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,21 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+from collections import Counter
+
+PRICE_TABLE = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+}
+
+OFFERS = {
+    "A": (3, 130),
+    "B": (2, 45),
+}
+
+
 def checkout(skus):
     raise NotImplementedError()
+
--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,6 +1,9 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
+
+
 def hello(friend_name):
     return f"Hello, {friend_name}!"
+
 
 
